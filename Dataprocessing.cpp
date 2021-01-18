0a55214fd718ad3a5fc936a5665b38837744314a
ankang1 <ankang1@AnkangGao.local>
2021-01-18 06:59:01 +0000

    tmp

--- Dataprocessing.cpp
+++ Dataprocessing.cpp
@@ -1,132 +1,46 @@
-#include "Dataprocessing.h"
-#include<cmath>
+#include <cmath>
 #include <iostream>
 #include <fstream>
 #include <string.h>
-#include <math.h>
-
-int filter_Gaussian(fftw_complex * spectral, double * omega, int Nfft, double sigma)
-{
-	//Gaussian filter
-	double Delta, temp, ftmp;
-	temp = sigma*sigma/2.;
-	for(int i=1;i<Nfft;i++)
-	{
-		ftmp = exp(-temp*omega[i]*omega[i]);
-		spectral[i][0] *= ftmp;
-		spectral[i][1] *= ftmp;
-	}
-	return 0;
-}
-
-STATUS derivatives1D(double * data, double * d_data, double Tlen, int Nfft, FilterType ftype, double sigma)
-{
-	//using FFT to get derivatives
-	//fftw initiates
-    fftw_complex *in, *out, *tilt_data;
-    in = (fftw_complex*) fftw_malloc(sizeof(fftw_complex) * Nfft);
-    out = (fftw_complex*) fftw_malloc(sizeof(fftw_complex) * Nfft);
-    tilt_data = (fftw_complex*) fftw_malloc(sizeof(fftw_complex) * Nfft);
-    if(NULL==in||NULL==out||NULL==tilt_data)
-    {
-    	return PROC_BAD_MEMORY;
-    }
-    fftw_plan pf, pb;
-    pf = fftw_plan_dft_1d(Nfft, in, out, FFTW_FORWARD, FFTW_MEASURE);
-	pb = fftw_plan_dft_1d(Nfft, in, out, FFTW_BACKWARD, FFTW_MEASURE);
-	if(Tlen<1.E-16) return PROC_SINGULAR;
-    double temp = 2.*M_PI/Tlen;
-    int i;
-    double *omega;
-    try
-	{
-		omega = new double[Nfft];
-	}
-	catch(const std::bad_alloc &ex)
-	{
-		return PROC_BAD_MEMORY;
-	}
-    for(i=0;i<Nfft;i++)
-    {
-       	if(i<=Nfft-i)
-    	{
-    		omega[i] = temp*double(i);
-    	}
-    	else
-    	{
-    		omega[i] = temp*double(i-Nfft);
-    	}
-    }
-    //execute FFT
-    //get spectral of data
-    for(i=0;i<Nfft;i++)
-    {
-    	in[i][0] = data[i];
-    	in[i][1] = 0.;
-    }
-    fftw_execute(pf); 
-    STATUS return_status;
-    return_status = filter(out, omega, Nfft, ftype);
-    if(PROC_SUCCESS != return_status)//filter
-    {
-    	return return_status;
-    }
-    for(i=0;i<Nfft;i++)
-    {
-    	tilt_data[i][0] = out[i][0]/double(Nfft);
-    	tilt_data[i][1] = out[i][1]/double(Nfft);
-    }
-    //get derivative of data
-    for(i=0;i<Nfft;i++)
-    {
-    	in[i][0] = -omega[i]*tilt_data[i][1];
-    	in[i][1] =  omega[i]*tilt_data[i][0];
-    }
-    fftw_execute(pb); 
-    for(i=0;i<Nfft;i++)
-    {
-    	d_data[i] = out[i][0];
-    }
-    //get double derivative of data
-    for(i=0;i<Nfft;i++)
-    {
-    	temp = -omega[i]*omega[i];
-    	in[i][0] = temp*tilt_data[i][0];
-    	in[i][1] = temp*tilt_data[i][1];
-    }
-    fftw_execute(pb); 
-    for(i=0;i<Nfft;i++)
-    {
-    	dd_data[i] = out[i][0];
-    }
-    //free allocated spaces
-	fftw_destroy_plan(pf);
-	fftw_destroy_plan(pb);
-    fftw_free(in); fftw_free(out);fftw_free(tilt_data);
-	return PROC_SUCCESS;
-}
-
+#include "Dataprocessing.h"
 
 GaussKernel::GaussKernel(double sigma, double dx, double cutoff) {
     m_maxOffset = std::floor(cutoff*sigma/dx);
-    double sum = 0.;
-    for(int i=-m_maxOffset; i<=m_maxOffset; ++i) {
+    std::vector<double> gauss(m_maxOffset + 1, 0.);
+    std::vector<double> sum(m_maxOffset + 1, 1.);
+    for(int i=0; i<=m_maxOffset; ++i) {
         double tmp = i*dx/sigma;
         tmp = std::exp(-0.5*tmp*tmp);
-        m_weight[i] = tmp;
-        sum += tmp;
+        gauss[i] = tmp;
+        if(i) {
+            sum[i] = sum[i-1] + 2. * gauss[i];
+        }
     }
-    for(int i=-m_maxOffset; i<=m_maxOffset; ++i) {
-        m_weight[i] /= sum;
+    double sum = 0.;
+    for(int i=-0; i<=m_maxOffset; ++i) {
+        std::vector<double> tmpw(i+1);
+        for(int j=0; j<=i; ++j) {
+            tmpw[j] = gauss[j]/sum[i];
+        }
+        m_weight.push_back(tmpw);
     }
 }
 
-double GaussKernel::GW(int offset) {
-    if(m_weight.count(offset)) {
-        return m_weight[offset];
-    } else {
-        return 0.;
+double GaussKernel::Sum(double *data, int padding) {
+    double sum = *data;
+    if(padding<0 || padding>m_maxOffset) {
+        printf("error: illegal padding %d, max[%d]\n", padding, m_maxOffset);
+        return sum;
     }
+    sum = (*data) * m_weight[padding][0];
+    for(int i=1; i<=padding; ++i) {
+        sum += m_weight[padding][i] * (*(data-i) + *(data+i));
+    }
+    return sum;
+}
+
+int GaussKernel::DoSmooth(std::vector<double> &data, std::vector<double> & sdata) {
+    ;
 }
 
 int GaussKernel::GetIMax() {
--- Dataprocessing.h
+++ Dataprocessing.h
@@ -1,40 +1,19 @@
 #ifndef DATAPROCESS_H
 #define DATAPROCESS_H
-#include <fftw3.h>
-#define PROC_SUCCESS 0
-#define PROC_CANNOT_OPENFILE -1001
-#define PROC_BAD_MEMORY -1002
-#define PROC_MIS_CALLED -1003
-#define PROC_SINGULAR -1004
-#ifndef STATUS
-	typedef int STATUS; 
-#endif
-enum FilterType
-{
-	eroundoff,
-	ebox,
-	eGaussian,
-	esharp,
-	eCauchy,
-	ePao,
-	eNone
-};
-STATUS loadfile(const char *filename, int &num, double * & time, double * & data);
-STATUS loadforcemomentumfile(const char * filename, int &num, double *&time, double *force_data[3], double *momentum_data[3]);
-STATUS findacmes(const double *data, const int num, int & numacmes, int * & acme_index);
-STATUS parser_arguments(int argc, char * argv[], char * filename, double & nperiods);
-STATUS find_maxspectral(fftw_complex * spectral, int Nfft, int firstn, int * & index);
-STATUS filter(fftw_complex * spectral, double * omega, int Nfft, FilterType type);
-STATUS derivatives12(double * data, double * d_data, double * dd_data, double Tlen, int Nfft, FilterType ftype = eGaussian);
-
+#include<vector>
 
 class GaussKernel {
 public:
     GaussKernel(double sigma, double dx, double cutoff = 5.);
-    double GW(int offset);
     int GetIMax();
-    std::map<int, double> m_weight;
+    std::vector<std::vector<double> > m_weight;
+	int DoSmooth(std::vector<double> &data, std::vector<double> & sdata);
+	// 0
+	// 0 1
+	// 0 1 2
     int m_maxOffset;
+private:
+	double Sum(double *data, int padding);
 };
 
 #endif
