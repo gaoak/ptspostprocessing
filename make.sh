ddd4bd76c32183ed90c9d00112820a9e6afb7a92
ankang <ankang@ubuntu.com>
2021-01-20 11:07:47 +0000

    add static lib

--- make.sh
+++ make.sh
@@ -1,4 +1,4 @@
 rm *.o process
 c++ -Og -c -I $TECINCLUDE *cpp -g
-c++ -Og -o process *.o $TECLIB/libtecio.a $TECLIB/libadkutil.a -lstdc++ -pthread -g
+c++ -Og -o process *.o libtecio.a libadkutil.a -lstdc++ -pthread -g
 ./process
