c33ee86353d312ea1b842706d8671cea875f6433
ankang <ankang@ubuntu.com>
2021-01-26 19:51:06 +0000

    update

--- Tecplotwraper.cpp
+++ Tecplotwraper.cpp
@@ -1,98 +1,8 @@
 #include<iostream>
-#include<fstream>
-#include<string>
-#include<vector>
 #include "Tecplotwraper.h"
 
-#ifdef __linux__
-#include<TECIO.h>
-#include<MASTER.h>
-int OutputTec360(const std::string filename, const std::vector<std::string> &variables,
-                 const std::vector<int> &N, const std::vector<void*> data,
-                 int isdouble,
-                 int debug,
-                 int filetype,
-                 int fileformat)
-{
-    std::string varlist = variables[0];
-    for(int i=1; i<variables.size(); ++i) {
-        varlist += "," + variables[i];
-    }
-    INTEGER4 Debug = debug;
-    INTEGER4 VIsDouble = isdouble;
-    INTEGER4 FileType = filetype;
-    INTEGER4 FileFormat = fileformat; // 0 == PLT, 1 == SZPLT
-    INTEGER4 I = 0; /* Used to track return codes */
-    /*
-    * Open the file and write the tecplot datafile
-    * header information
-    */
-    I = TECINI142((char*)"OutputTec360", // dataset title, seems not important
-                (char*)varlist.c_str(),  // variables list
-                (char*)filename.c_str(), // output filename
-                (char*)".", // Scratch Directory
-                &FileFormat,
-                &FileType,
-                &Debug,
-                &VIsDouble);
-    if(I) return -1;
-
-    /*Ordered Zone Parameters*/
-    INTEGER4 IMax = N[0];
-    INTEGER4 JMax = N[1];
-    INTEGER4 KMax = N[2];
-    INTEGER4 ZoneType = 0;
-    INTEGER4 ICellMax = 0;
-    INTEGER4 JCellMax = 0;
-    INTEGER4 KCellMax = 0;
-    double   SolTime  = 0.;
-    INTEGER4 StrandID = 0;
-    INTEGER4 ParentZn = 0;
-    INTEGER4 IsBlock = 1;
-    INTEGER4 NFConns = 0;
-    INTEGER4 FNMode = 0;
-    INTEGER4 TotalNumFaceNodes = 1;
-    INTEGER4 TotalNumBndryFaces = 1;
-    INTEGER4 TotalNumBndryConnections = 1;
-    INTEGER4 ShrConn = 0;
-    I = TECZNE142((char*)"Ordered Zone", // zone name, seems not important
-                &ZoneType, // 0 is ordered zone
-                &IMax,
-                &JMax,
-                &KMax,
-                &ICellMax,
-                &JCellMax,
-                &KCellMax,
-                &SolTime,
-                &StrandID,
-                &ParentZn,
-                &IsBlock,
-                &NFConns,
-                &FNMode,
-                &TotalNumFaceNodes,
-                &TotalNumBndryFaces,
-                &TotalNumBndryConnections,
-                NULL,
-                NULL,
-                NULL,
-                &ShrConn);
-    if(I) return -1;
-
-    INTEGER4 III = IMax * JMax * KMax;
-    for(int i=0; i<data.size(); ++i) {
-        I = TECDAT142(&III, data[i], &VIsDouble);
-        if(I) return -1;
-    }
-    I = TECEND142();
-    if(I) return -1;
-    
-    return 0;
-}
-
-#else
-
-int OutputTec360(const std::string filename, const std::vector<std::string> &variables,
-                 const std::vector<int> &N, const std::vector<void*> data,
+int OutputTec360_ascii(const std::string filename, const std::vector<std::string> &variables,
+                 const std::vector<int> &N, const std::vector<std::vector<double> > data,
                  int isdouble,
                  int debug,
                  int filetype,
@@ -108,11 +18,223 @@ int OutputTec360(const std::string filename, const std::vector<std::string> &var
     int Np = N[0] * N[1] * N[2];
     for(int i=0; i<Np; ++i) {
         for(int j=0; j<data.size(); ++j) {
-            ofile << ((double*)data[j])[i] << " ";
+            ofile << data[j][i] << " ";
         }
         ofile << "\n";
     }
     ofile.close();
     return 0;
 }
-#endif
+
+int BinaryWrite(std::ofstream &ofile, std::string str) {
+    int tmp = 0;
+    for(int i=0; i<str.size(); ++i) {
+        tmp = str[i];
+        ofile.write((char*)&tmp, 4);
+    }
+    ofile.write((char*)&tmp, 4);
+}
+
+int OutputTec360_binary(const std::string filename, const std::vector<std::string> &variables,
+                 const std::vector<int> &N, const std::vector<std::vector<double> > data,
+                 int isdouble,
+                 int debug,
+                 int filetype,
+                 int fileformat)
+{
+    std::ofstream odata;
+    odata.open(filename, std::ios::binary);
+	if(!odata.is_open())
+	{
+        printf("error unable to open file %s\n", filename.c_str());
+		return -1;
+	}
+    char tecplotversion[] = "#!TDV112";
+	odata.write((char*)tecplotversion, 8);
+    int value1 = 1;
+	odata.write((char*)&value1, 4);
+    int filetype = 0;
+	odata.write((char*)&filetype, 4);
+	//read file title and variable names
+	int tempi = 0;
+    std::string filetitle = "";
+    BinaryWrite(odata, filetitle);
+    int nvar = variables.size();
+    odata.write((char*)&nvar, 4);//number of variables
+    std::vector<std::string> vartitle;
+	for(int i=0; i<nvar; i++)
+	{
+        BinaryWrite(odata, variables[i]);
+	}
+    float marker299I = 299.0f;
+	odata.write((char*)&marker299I, 4);
+	//zone title
+    std::string zonetitle("ZONE 0");
+	BinaryWrite(odata, zonetitle);
+    int parentzone = -1;
+	odata.write((char*)&parentzone, 4);
+    int strandid = -1;
+	odata.write((char*)&strandid, 4);
+    double soltime = 0.0;
+    odata.write((char*)&soltime, 8);
+    int unused = -1;
+    odata.write((char*)&unused, 4);
+    int zonetype = 0;
+    odata.write((char*)&zonetype, 4);
+    int zero = 0;
+    odata.write((char*)&zero, 4);
+    odata.write((char*)&zero, 4);
+    odata.write((char*)&zero, 4);
+	for(int i=0; i<3; ++i) {
+        int tmp = N[i];
+        odata.write((char*)&tmp, 4);
+    }
+
+    odata.write((char*)&zero, 4);
+    float marker357 = 357.0f;
+	odata.write((char*)&marker357, 4);
+    float marker299II = 299.0f;
+	odata.write((char*)&marker299II, 4);
+	std::vector<int> binarydatatype(nvar, 2);
+	odata.write((char*)binarydatatype.data(), 4*nvar);
+    odata.write((char*)&zero, 4);
+    odata.write((char*)&zero, 4);
+    int minus1 = -1;
+    odata.write((char*)&minus1, 4);
+
+    for(int i=0; i<nvar; ++i) {
+        double minv = 0., maxv=1.;
+        odata.write((char*)&minv, 8);
+        odata.write((char*)&maxv, 8);
+    }
+
+    int datanumber, datasize;
+	datanumber = N[0] * N[1] * N[2];
+	datasize = N[0] * N[1] * N[2] * 8;
+    std::vector<float> vardata(datanumber);
+    for(int i=0; i<nvar; ++i) {
+        odata.write((char*)data[i].data(), datasize);
+    }
+    return 0;
+}
+
+int InputTec360_binary(const std::string filename, std::vector<std::string> &variables,
+                 std::vector<int> &N, std::vector<std::vector<double> > data,
+                 int &isdouble,
+                 int debug,
+                 int filetype,
+                 int fileformat) {
+	std::ifstream indata;
+    indata.open(filename, std::ios::binary);
+	if(!indata.is_open())
+	{
+        printf("error unable to open file %s\n", filename.c_str());
+		return -1;
+	}
+    char tecplotversion[8];
+	indata.read((char*)tecplotversion, 8);
+    int value1;
+	indata.read((char*)&value1, 4);
+    int filetype;
+	indata.read((char*)&filetype, 4);
+	//read file title and variable names
+	int tempi;
+    std::string filetitle;
+	while(1)
+	{
+        indata.read((char*)&tempi, 4);
+        if(tempi==0) break;
+        char c = tempi;
+        filetitle.push_back(c);
+	}
+    int nvar;
+    indata.read((char*)&nvar, 4);//number of variables
+    variables.clear();
+	for(int i=0; i<nvar; i++)
+	{
+        std::string vname;
+        while(1)
+        {
+            indata.read((char*)&tempi, 4);
+            if(tempi==0) break;
+            char c = tempi;
+            vname.push_back(c);
+        }
+        variables.push_back(vname);
+	}
+    float marker299I;
+	indata.read((char*)&marker299I, 4);
+	if(marker299I!=299.)
+	{
+		printf("error in reading file %s\n", filename.c_str());
+		return -1;
+	}
+	//zone title
+    std::string zonentitle;
+	while(1)
+	{
+        indata.read((char*)&tempi, 4);
+        if(tempi==0) break;
+        char c = tempi;
+        zonentitle.push_back(c);
+	}
+    int parentzone;
+	indata.read((char*)&parentzone, 4);
+    int strandid;
+	indata.read((char*)&strandid, 4);
+    double soltime;
+    indata.read((char*)&soltime, 8);
+    int unused;
+    indata.read((char*)&unused, 4);
+    int zonetype;
+    indata.read((char*)&zonetype, 4);
+    int zero;
+    indata.read((char*)&zero, 4);
+    indata.read((char*)&zero, 4);
+    indata.read((char*)&zero, 4);
+    N.resize(3);
+	indata.read((char*)N.data(), 3*4);
+    indata.read((char*)&zero, 4);
+    float marker357;
+	indata.read((char*)&marker357, 4);
+    float marker299II;
+	indata.read((char*)&marker299II, 4);
+	if(marker357!=357.||marker299II!=299.)
+	{
+		printf("error in reading file %s\n", filename.c_str());
+		return -1;
+	}
+	int * binarydatatype = new int[nvar];
+	indata.read((char*)binarydatatype, 4*nvar);
+    isdouble = binarydatatype[0] == 2;
+    indata.read((char*)&zero, 4);
+    indata.read((char*)&zero, 4);
+    int minus1;
+    indata.read((char*)&minus1, 4);
+
+    for(int i=0; i<nvar; ++i) {
+        double minv, maxv;
+        indata.read((char*)&minv, 8);
+        indata.read((char*)&maxv, 8);
+    }
+
+    int datanumber, datasize;
+	datanumber = N[0] * N[1] * N[2];
+	datasize = N[0] * N[1] * N[2] * 4;
+    for(int i=0; i<nvar; ++i) {
+        if(isdouble) {
+            std::vector<double> vardata(datanumber);
+            indata.read((char*)vardata.data(), datasize * 2);
+            data.push_back(vardata);
+        } else {
+            std::vector<float> vardata(datanumber);
+            std::vector<double> doublevardata(datanumber);
+            indata.read((char*)vardata.data(), datasize);
+            for(int i=0; i<datanumber; ++i) {
+                doublevardata[i] = vardata[i];
+            }
+            data.push_back(doublevardata);
+        }
+    }
+    return 0;
+}
--- Tecplotwraper.h
+++ Tecplotwraper.h
@@ -2,15 +2,28 @@
 #define TECPLOTWRAPER_H
 #include<string>
 #include<vector>
-#ifdef __linux__
-#define TECPLOTEXT ".plt"
-#else
-#define TECPLOTEXT ".dat"
-#endif
-int OutputTec360(const std::string filename, const std::vector<std::string> &variables,
-                 const std::vector<int> &N, const std::vector<void*> data,
+#include<fstream>
+
+int BinaryWrite(std::ofstream &ofile, std::string str);
+
+int OutputTec360_ascii(const std::string filename, const std::vector<std::string> &variables,
+                 const std::vector<int> &N, const std::vector<std::vector<double> > data,
                  int isdouble,
-                 int debug = 0,
-                 int filetype = 0,
-                 int fileformat = 0);
+                 int debug,
+                 int filetype,
+                 int fileformat);
+
+int OutputTec360_binary(const std::string filename, const std::vector<std::string> &variables,
+                 const std::vector<int> &N, const std::vector<std::vector<double> > data,
+                 int isdouble,
+                 int debug,
+                 int filetype,
+                 int fileformat);
+
+int InputTec360_binary(const std::string filename, std::vector<std::string> &variables,
+                 std::vector<int> &N, std::vector<std::vector<double> > data,
+                 int &isdouble,
+                 int debug,
+                 int filetype,
+                 int fileformat);
 #endif
