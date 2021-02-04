aa119112d1f04a2aea92f1848dc6528662cb25e7
ankang <ankang@ubuntu.com>
2021-02-04 17:49:55 +0000

    rm make.sh

--- include/make.sh
+++
@@ -1,3 +0,0 @@
-rm *.o process
-c++ -std=c++11 -DMAKEBASH -c *cpp -Og -g
-c++ -std=c++11 -DMAKEBASH -o process *.o  -lstdc++ -Og -g
--- include/makeRelease.sh
+++
@@ -1,3 +0,0 @@
-rm *.o process
-c++ -std=c++11 -DMAKEBASH -c *cpp -O3
-c++ -std=c++11 -DMAKEBASH -o process *.o -lstdc++ -O3
