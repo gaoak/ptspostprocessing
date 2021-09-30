1dd51e0b45dfd32bf82484d34b6ca9a862cd491c
Ankang Gao <ankang.gao@imperial.ac.uk>
2021-09-30 12:14:55 +0100

    fix bug

--- stabilitymode.cpp
+++ stabilitymode.cpp
@@ -28,7 +28,7 @@ int main(int argc, char *argv[]) {
     if (modeflow.GetNumPhys()==2) {
         modeflow.AddPhysics("w", (void*)func);
     }
-    for (int f=0; f<baseflow.GetNumPhys(); ++f) {
+    for (int f=0; f<baseflow.GetNumPhys() - 1; ++f) { //no p
         for (int p=0; p < baseflow.GetTotPoints(); ++p) {
             double v = baseflow.GetPhysValue(f, p) + amplitude * modeflow.GetPhysValue(f, p);
             modeflow.SetPhysValue(v, f, p);
