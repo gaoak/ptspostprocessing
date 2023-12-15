d6022f3f869c59347fec38d74b7e02409c810138
Ankang Gao <ankanggao@ustc.edu.cn>
2023-12-15 16:09:52 +0800

    update the code

--- processLag.cpp
+++ processLag.cpp
@@ -30,7 +30,12 @@ void SelectPoint(IncFlow & fieldData, int &maxid) {
   }
 }
 
-void GetVorticityTerms(IncFlow & fieldData, int maxid, std::vector<double> & vorticity, std::vector<double> &mBomega, std::vector<double> & curla) {
+void GetVorticityTerms(IncFlow & fieldData, int maxid, std::vector<double> & vorticity, std::vector<double> &Bsurf, std::vector<double> &mBomega, std::vector<double> & curla) {
+  double nu = 1/800.;
+  vorticity.resize(3);
+  mBomega.resize(3);
+  curla.resize(3);
+  Bsurf.resize(9);
   double ux = fieldData.GetPhys(fieldData.GetPhysID("u_x"))[maxid];
   double uy = fieldData.GetPhys(fieldData.GetPhysID("u_y"))[maxid];
   double uz = fieldData.GetPhys(fieldData.GetPhysID("u_z"))[maxid];
@@ -41,12 +46,8 @@ void GetVorticityTerms(IncFlow & fieldData, int maxid, std::vector<double> & vor
   double wy = fieldData.GetPhys(fieldData.GetPhysID("w_y"))[maxid];
   double wz = fieldData.GetPhys(fieldData.GetPhysID("w_z"))[maxid];
   double Lu = fieldData.GetPhys(fieldData.GetPhysID("Lu"))[maxid];
-  double Lu = fieldData.GetPhys(fieldData.GetPhysID("Lv"))[maxid];
-  double Lu = fieldData.GetPhys(fieldData.GetPhysID("Lw"))[maxid];
-  vorticity.resize(3);
-  mBomega.resize(3);
-  curla.resize(3);
-  double nu = 1/800.;
+  double Lv = fieldData.GetPhys(fieldData.GetPhysID("Lv"))[maxid];
+  double Lw = fieldData.GetPhys(fieldData.GetPhysID("Lw"))[maxid];
   curla[0] = nu*Lu;
   curla[1] = nu*Lv;
   curla[2] = nu*Lw;
@@ -54,48 +55,113 @@ void GetVorticityTerms(IncFlow & fieldData, int maxid, std::vector<double> & vor
   vorticity[1] = uz - wx;
   vorticity[2] = vx - uy;
   double theta = ux + vy + wz;
-  mBomega[0] = (ux - theta) * vorticity[0] +
-               (uy        ) * vorticity[1] +
-               (uz        ) * vorticity[2];
-  mBomega[1] = (vx        ) * vorticity[0] +
-               (vy - theta) * vorticity[1] +
-               (vz        ) * vorticity[2];
-  mBomega[2] = (wx        ) * vorticity[0] +
-               (wy        ) * vorticity[1] +
-               (wz - theta) * vorticity[2];
+  Bsurf[0] = theta - ux;
+  Bsurf[1] =       - uy;
+  Bsurf[2] =       - uz;
+  Bsurf[3] =       - vx;
+  Bsurf[4] = theta - vy;
+  Bsurf[5] =       - vz;
+  Bsurf[6] =       - wx;
+  Bsurf[7] =       - wy;
+  Bsurf[8] = theta - wz;
+  for(int d=0; d<3; ++d) {
+    int i= 3*d;
+    mBomega[d] = - Bsurf[i] * vorticity[0] - Bsurf[i+1] * vorticity[1] - Bsurf[i+2] * vorticity[2];
+  }
 }
 
-void GetCoordsTerms(IncFlow & fieldData, int maxid, std::vector<double> &Xcoords, std::vector<double> &Ycoords, std::vector<double> &Zcoords) {
+void GetCoords(IncFlow & fieldData, int maxid, std::vector<double> &coords, std::vector<double> &Xcoords, std::vector<double> &Ycoords, std::vector<double> &Zcoords) {
+  coords.resize(3, 0.);
   Xcoords.resize(6, 0.);
   Ycoords.resize(6, 0.);
   Zcoords.resize(6, 0.);
-  double x = fieldData.GetPhys(fieldData.GetPhysID("x"));
-  double y = fieldData.GetPhys(fieldData.GetPhysID("y"));
-  double z = fieldData.GetPhys(fieldData.GetPhysID("z"));
+  std::vector<int> Np = fieldData.GetN();
   std::vector<int> maxindex;
   invIndex(Np, maxid, maxindex);
-  std::vector<double> Xcoords(6, 0.), Ycoords(6, 0.), Zcoords(6, 0.);
   for(int d=0; d<3; ++d) {
     std::vector<int> index = maxindex;
+    int i = Index(Np, index);
+    coords[d] = fieldData.GetCoordValue(d, i);
     index[d] -= 1;
     int im = Index(Np, index);
-    Xcoords[d*2] = x[im]; Ycoords[d*2] = y[im]; Zcoords[d*2] = z[im];
+    Xcoords[d*2] = fieldData.GetCoordValue(0, im);
+    Ycoords[d*2] = fieldData.GetCoordValue(1, im);
+    Zcoords[d*2] = fieldData.GetCoordValue(2, im);
     index = maxindex;
     index[d] += 1;
     int ip = Index(Np, index);
-    Xcoords[2*d+1] = x[ip]; Ycoords[2*d+1] = y[ip]; Zcoords[2*d+1] = z[ip];
+    Xcoords[2*d+1] = fieldData.GetCoordValue(0, ip);
+    Ycoords[2*d+1] = fieldData.GetCoordValue(1, ip);
+    Zcoords[2*d+1] = fieldData.GetCoordValue(2, ip);
+  }
+}
+
+void GetGradCoords(std::vector<double> &dx, std::vector<double> &Xcoords, std::vector<double> &Ycoords, std::vector<double> &Zcoords, std::vector<double> &F) {
+  F.resize(9, 0.);
+  for(int d=0; d<3; ++d) {
+    F[0+d] = (Xcoords[2*d+1] - Xcoords[2*d]) / (2. * dx[d]);
+    F[3+d] = (Ycoords[2*d+1] - Ycoords[2*d]) / (2. * dx[d]);
+    F[6+d] = (Zcoords[2*d+1] - Zcoords[2*d]) / (2. * dx[d]);
   }
 }
 
 int main(int argc, char* argv[]) {
-  string filenameformat;
+  IncFlow fieldData;
+  string filenameformat("Points");
   if(argc>1) {
     filenameformat = argv[1];
   }
-  IncFlow fieldData;
-  fieldData.InputData("Points_R00000_T000.000000.plt");
-  int maxid = 0;
-  SelectPoint(fieldData, maxid);
-
+  filenameformat += "_R%05d_T%010.6lf.plt";
+  char buff[100];
+  double time = 0., dt = 0.001953125;
+  std::vector<double> dx0;
+    int maxid = 0;
+  for(int n=0; n<=2048; ++n) {
+    time = dt *n;
+    sprintf(buff, filenameformat.c_str(), 0, time);
+    string filename(buff);
+    fieldData.InputData(filename);
+    if(n==0) {
+      dx0 = fieldData.GetDetx();
+      SelectPoint(fieldData, maxid);
+    }
+    cout << time << " ";
+    std::vector<double>  vorticity;
+    std::vector<double>  Bsurf;
+    std::vector<double>  mBomega;
+    std::vector<double>  curla;
+    GetVorticityTerms(fieldData, maxid, vorticity, Bsurf, mBomega, curla);
+    std::vector<double> coords;
+    std::vector<double> Xcoords;
+    std::vector<double> Ycoords;
+    std::vector<double> Zcoords;
+    GetCoords(fieldData, maxid, coords, Xcoords, Ycoords, Zcoords);
+    std::vector<double> F;
+    GetGradCoords(dx0, Xcoords, Ycoords, Zcoords, F);
+    for(auto v:coords) {
+      cout << v << " ";
+    }
+    cout << -9999 << " ";
+    for(auto v:vorticity) {
+      cout << v << " ";
+    }
+    cout << -9999 << " ";
+    for(auto v:mBomega) {
+      cout << v << " ";
+    }
+    cout << -9999 << " ";
+    for(auto v:curla) {
+      cout << v << " ";
+    }
+    cout << -9999 << " ";
+    for(auto v:Bsurf) {
+      cout << v << " ";
+    }
+    cout << -9999 << " ";
+    for(auto v:F) {
+      cout << v << " ";
+    }
+    cout << endl;
+  }
   return 0;
 }
