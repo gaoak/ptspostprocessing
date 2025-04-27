696bb1b75896085d9f49bc37114be861ff7c9b18
Ankang Gao <ankanggao@ustc.edu.cn>
2025-04-27 14:34:33 +0800

    fix a dimension error

--- src/LineData.cpp
+++ src/LineData.cpp
@@ -54,7 +54,7 @@ int LineData::RemovePhysics(int i) {
 }
 
 int LineData::InterpolateFrom(const StructuredData & origin, std::map<int,double> field) {
-    std::vector<double> x(origin.GetNumCoords(), 0.);
+    std::vector<double> x(3, 0.);
     std::vector<std::vector<double>> data(field.size());
     for(int i=0; i<m_Np; ++i) {
         for(size_t d=0; d<m_x.size(); ++d) {
